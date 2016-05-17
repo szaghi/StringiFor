a4c034be33c16c4ebb539897b3eac5b5b3432315
Stefano Zaghi <stefano.zaghi@gmail.com>
2016-05-17 10:50:09 +0200

    more verbose tests output

--- run_tests.sh
+++ run_tests.sh
@@ -18,6 +18,11 @@ for e in $( ls -F -a ./exe/ | grep "[*]$" ); do
   is_passed=`./exe/$e | grep -i "Are all tests passed? " | awk '{print $5}'`
   tests_executed=("${tests_executed[@]}" "$is_passed")
   echo "  run test $e, is passed? $is_passed"
+  if [ "$is_passed" == 'F' ]; then
+    echo
+    echo "Test failed"
+    ./exe/$e
+  fi
 done
 passed=$(all_passed tests_executed)
 echo "Number of tests executed ${#tests_executed[@]}"
