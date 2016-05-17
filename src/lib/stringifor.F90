499ed18e0b12753fa233c86399ca992a593927d7
Stefano Zaghi <stefano.zaghi@gmail.com>
2016-05-17 14:47:12 +0200

    Add read_line(s) methods
    Add read_line(s) methods: read single line, and all lines of a formatted
    connected unit, both as TBP or standalone procedure.

--- src/lib/stringifor.F90
+++ src/lib/stringifor.F90
@@ -23,6 +23,8 @@ public :: CK
 public :: string
 ! expose StingiFor overloaded builtins
 public :: adjustl, adjustr, index, len, len_trim, repeat, scan, trim
+! expose StingiFor new procedures
+public :: read_lines
 ! expose PENF kinds
 public :: I1P, I2P, I4P, I8P, R4P, R8P, R16P
 !-----------------------------------------------------------------------------------------------------------------------------------
@@ -62,4 +64,49 @@ interface trim
   module procedure strim
 endinterface trim
 !-----------------------------------------------------------------------------------------------------------------------------------
+contains
+  subroutine read_lines(unit, lines, iostat, iomsg)
+  !---------------------------------------------------------------------------------------------------------------------------------
+  !< Read lines (records) from a connected-formatted unit.
+  !<
+  !< @note The connected unit is rewinded. At a successful exit current record is at eof, at the beginning otherwise.
+  !<
+  !< The lines are returned as an array of strings that are read until the eof is reached.
+  !< The line is read as an ascii stream read until the eor is reached.
+  !---------------------------------------------------------------------------------------------------------------------------------
+  integer,          intent(in)               :: unit     !< Logical unit.
+  type(string),     intent(out), allocatable :: lines(:) !< The lines.
+  integer,          intent(out),   optional  :: iostat   !< IO status code.
+  character(len=*), intent(inout), optional  :: iomsg    !< IO status message.
+  integer                                    :: iostat_  !< IO status code, local variable.
+  character(len=:),          allocatable     :: iomsg_   !< IO status message, local variable.
+  integer                                    :: l        !< Counter.
+  !---------------------------------------------------------------------------------------------------------------------------------
+
+  !---------------------------------------------------------------------------------------------------------------------------------
+  iomsg_ = repeat(' ', 99) ; if (present(iomsg)) iomsg_ = iomsg
+  rewind(unit)
+  l = 0
+  do
+    read(unit, *, err=10, end=10)
+    l = l + 1
+  enddo
+  10 rewind(unit)
+  if (l>0) then
+    allocate(lines(1:l))
+    l = 0
+    iostat_ = 0
+    do
+      l = l + 1
+      call lines(l)%read_line(unit=unit, iostat=iostat_, iomsg=iomsg_)
+      if (iostat_/=0.and..not.is_iostat_eor(iostat_)) then
+        exit
+      endif
+    enddo
+  endif
+  if (present(iostat)) iostat = iostat_
+  if (present(iomsg)) iomsg = iomsg_
+  return
+  !---------------------------------------------------------------------------------------------------------------------------------
+  endsubroutine read_lines
 endmodule stringifor
